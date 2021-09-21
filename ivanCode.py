ea1c98543557875de44d0baf55e50e73db4d9588
edhounter <91148422+edhounter@users.noreply.github.com>
2021-09-21 19:46:37 +0300

    Update ivanCode.py

--- ivanCode.py
+++ ivanCode.py
@@ -22,7 +22,18 @@ def CreateTrainingData():
 
 
 CreateTrainingData()  # загалом, даний блок є блоком підготовки данних до обробки
-
+def CreateTestingData():
+    for category in Categories_test: 
+        path = os.path.join(DATADIR,category) # шлях до папок "OneBand" та "TwoBands"
+        class_num=Categories_test.index(category) 
+        try:
+            for img in os.listdir(path):
+                img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                testing_data.append([new_array ,class_num])
+        except Exception as e:
+          pass
+CreateTestingData()
 import random
 
 random.shuffle(training_data)
@@ -33,7 +44,14 @@ for feature, label in training_data:
 y.append(label)
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 y = np.array(y).reshape(-1)
-
+random.shuffle(testing_data) 
+X_test=[]
+y_test=[]
+for feature,label in testing_data:
+    X_test.append(feature)
+    y_test.append(label)
+X_test=np.array(X_test).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+y_test=np.array(y_test).reshape(-1)
 # розділення данних на вхідні (Х) та вихідні (y)
 import tensorflow as tf
 
@@ -77,3 +95,4 @@ matplotlib.pyplot.plot(history.history['val_accuracy'], label = 'val_accuracy')
 matplotlib.pyplot.plot(history.history['accuracy'], label = 'accuracy')
 matplotlib.pyplot.title('Training and validation accuracy on Data500')
 matplotlib.pyplot.legend()
+model.evaluate(X_test, y_test)
